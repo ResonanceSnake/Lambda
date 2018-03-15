80b0e6ce176a1d87042bc4412ca11a10c35e4fd1
Jake Martin <jacobmartin@me.com>
2018-03-14 22:33:19 -0500

    true false lambdas added

--- Lambda/Program.fs
+++ Lambda/Program.fs
@@ -6,6 +6,16 @@
 [<EntryPoint>]
 let main argv = 
     Console.WriteLine("Lambdas will go here")
+    let True = fun x -> fun y -> x
+    let False = fun x -> fun y -> y
+
+    let trueValue = True(true)(false)
+    let falseValue = False(true)(false)
+    let If = fun x -> fun y -> fun z -> x(y)(z)
+    
+    printfn "%s" (If(True)("TRUE")("oops.."))
+    printfn "%s" (If(False)("oops")("FALSE"))
+
 
     Console.WriteLine("Press any key...")
     Console.ReadKey() |> ignore
