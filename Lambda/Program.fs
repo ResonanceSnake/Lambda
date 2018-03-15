3849efcf3355ddc75fa4e7eedc23c22f0cd3b4eb
Jake Martin <jacobmartin@me.com>
2018-03-14 21:48:38 -0500

    First commit

---
+++ Lambda/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    Console.WriteLine("Lambdas will go here")
+
+    Console.WriteLine("Press any key...")
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
